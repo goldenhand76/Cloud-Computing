9c6ed28e37e0d02f808d3e9011ecca6dc83f33d2
Reza Zarin <rezazarindast76@gmail.com>
2026-06-14 06:56:49 +0330

    :tada: users app created

---
+++ users/api/server.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"cloud-computing/users/util"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
+}
